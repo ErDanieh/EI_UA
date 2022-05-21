f0e7b808476bcfc327a5bb3b01fa3777982712cf
Daniel <71588528+ErDanieh@users.noreply.github.com>
2022-05-21 13:21:15 +0200

    Buscador faltan métodos

---
+++ Práctica/Buscador/lib/buscador.cpp
@@ -0,0 +1,203 @@
+#include "buscador.h"
+#include "indexadorHash.h"
+
+ResultadoRI::ResultadoRI(const double &sim, const long int &idD, const int &np)
+{
+    this->vSimilitud = sim;
+    this->idDoc = idD;
+    this->numPregunta = np;
+}
+ResultadoRI::ResultadoRI(const ResultadoRI &copiar)
+{
+    this->vSimilitud = copiar.vSimilitud;
+    this->idDoc = copiar.idDoc;
+    this->numPregunta = copiar.numPregunta;
+}
+
+bool ResultadoRI::operator<(const ResultadoRI &lhs) const
+{
+    if (numPregunta == lhs.numPregunta)
+        return (vSimilitud < lhs.vSimilitud);
+    else
+        return (numPregunta > lhs.numPregunta);
+}
+
+ostream &operator<<(std::ostream &os, const ResultadoRI &resultado)
+{
+    return os << resultado.vSimilitud << "\t\t" << resultado.idDoc << "\t" << resultado.numPregunta << "\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Buscador
+
+Buscador::Buscador(const std::string &directorioIndexacion, const int &f) : IndexadorHash(directorioIndexacion),
+                                                                            formSimilitud(0),
+                                                                            c(2),
+                                                                            k1(1.2),
+                                                                            b(0.75)
+{
+}
+
+Buscador::Buscador() : IndexadorHash()
+{
+}
+
+Buscador::~Buscador()
+{
+    priority_queue<ResultadoRI> vacia;
+    swap(this->docsOrdenados, vacia);
+}
+
+Buscador::Buscador(const Buscador &b) : IndexadorHash(b)
+{
+    this->formSimilitud = b.formSimilitud;
+    this->c = b.c;
+    this->k1 = b.k1;
+    this->b = b.b;
+}
+
+Buscador &Buscador::operator=(const Buscador &cop)
+{
+    if (this != &cop)
+    {
+        (*this).~Buscador();
+        Buscador(cop);
+    }
+    return *this;
+}
+
+void Buscador::CambiarParametrosBM25(const double &kk1, const double &kb)
+{
+    this->k1 = kk1;
+    this->b = kb;
+}
+
+void Buscador::DevolverParametrosBM25(double &kk1, double &kb) const
+{
+    kk1 = this->k1;
+    kb = this->b;
+}
+
+void Buscador::CambiarParametrosDFR(const double &kc)
+{
+    this->c = kc;
+}
+
+double Buscador::DevolverParametrosDFR() const
+{
+    return this->c;
+}
+
+bool Buscador::CambiarFormulaSimilitud(const int &f)
+{
+    if (f == 0 || f == 1)
+    {
+        this->formSimilitud = f;
+        return true;
+    }
+    return false;
+}
+
+int Buscador::DevolverFormulaSimilitud() const
+{
+    return this->formSimilitud;
+}
+
+bool Buscador::SeleccionarMetodo(const int &numDocumentos, const int &numPregunta)
+{
+    // Tenemos que comprobar que el indice de preguntas no esta vacio
+    // En el caso de que este vacio no podemos realizar la busqueda y devolvemos false
+    if (getIndicePregunta().empty())
+        return false;
+    else
+    {
+        if (this->formSimilitud == 0)
+            realizarDFR(numDocumentos, numPregunta);
+        else
+            realizarBM25(numDocumentos, numPregunta);
+        return true;
+    }
+}
+
+bool Buscador::Buscar(const int &numDocumentos)
+{
+    priority_queue<ResultadoRI> empty;
+    swap(this->docsOrdenados, empty);
+    return SeleccionarMetodo(numDocumentos, 0);
+}
+
+bool Buscador::Buscar(const string &dirPreguntas, const int &numDocumentos, const int &numPregInicio, const int &numPregFin)
+{
+    // Comprobamos que el directorio con las preguntas existe sacando su información
+    struct stat dir;
+    if (stat(dirPreguntas.c_str(), &dir) == -1 || !S_ISDIR(dir.st_mode))
+    {
+        return false;
+    }
+    else
+    {
+        // Vaciamos la colección de documentos ordenado
+        priority_queue<ResultadoRI> empty;
+        swap(this->docsOrdenados, empty);
+        ifstream fichPregunta;
+        string rutaFichero;
+        for (int i = numPregInicio; i <= numPregFin; i++)
+        {
+            // Creamos la ruta del fichero que queremos abrir
+            rutaFichero = dirPreguntas + to_string(i) + ".txt";
+            fichPregunta.open(rutaFichero.c_str());
+            if (fichPregunta.is_open())
+            {
+                // Cogemos la pregunta de la primera linea
+                string pregunta;
+                getline(fichPregunta, pregunta);
+                fichPregunta.close();
+                // Indexamos la pregunta
+                IndexarPregunta(pregunta);
+                // Seleccionamos el metodo que queremos realizar y lo realizamos
+                bool realizado = SeleccionarMetodo(numDocumentos, i);
+                if (!realizado)
+                    return false;
+            }
+        }
+        // Todo se ha realizado correctamente
+        return true;
+    }
+}
+
+void Buscador::realizarDFR(const int &numDocumentos, const int &numPregunta)
+{
+    double vSimilitud = 0.0;
+    double wiq = 0.0;    // peso en la query del termino i de la query q
+    double wid = 0.0;    // peso en el documento del término i de la query q
+    double ft = 0.0;     // número total de veces que el término t aparece en toda la colección
+    double ftd = 0.0;    // número de veces que el término t aparece en el documento d
+    double lambda = 0.0; // es la razón entre la frecuencia del término en la colección y la cantidad de documentos en la colección
+    double first = 0.0;
+    double avg = 0.0; //media en palabras (no de parada) de los documentos
+
+    avg = (double) informacionColeccionDocs.getNumTotalPalSinParada() / (double) informacionColeccionDocs.getNumDocs();
+    unordered_map<string, InformacionTerminoPregunta>::iterator infTermPregunta;
+    unordered_map<string, InformacionTermino>::iterator informacionTermino;
+    unordered_map<long, InfTermDoc>::iterator terminoDoc;
+    unordered_map<string,InfDoc>::iterator informacionDoc;
+
+}
+void Buscador::realizarBM25(const int &numDocumentos, const int &numPregunta) {}
+void Buscador::ImprimirResultadoBusqueda(const int &numDocumentos) const {}
+bool Buscador::ImprimirResultadoBusqueda(const int &numDocumentos, const string &nombreFichero) const {}
+
+ostream &operator<<(ostream &s, const Buscador &p)
+{
+    string preg;
+    s << "Buscador : " << endl;
+    if (p.DevuelvePregunta(preg))
+        s << "\tPregunta indexada : " << preg << endl;
+    else
+        s << "\tNo hay ninguna pregunta indexada" << endl;
+
+    s << "\tDatos del indexador : " << endl
+      << (IndexadorHash)p; // Invoca a la sobrecarga de la salida del Indexador
+
+    return s;
+}
